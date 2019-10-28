b7c6436d87802a8bb0c17552e0a1aaf5644e0a5e
REné-Marcel Zadi <rene.mzadi45@gmail.com>
2019-10-28 10:31:58 +0100

    My simpson-quote done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import QuoteCitation from './Components/QuoteCitation';
+
+import axios from 'axios';
+
+const exemple = [{
+  quote:"Why are you pleople avoiding me? Does my withered face remind you of the grim specter of death?",
+  character:"Abe Simpson",
+  image:"https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FAbrahamSimpson.png?1497567511593"
+}]
+
+
+class App extends React.Component{
+
+state={
+  simpson:exemple[0]
+}
+
+getSimpson= this.getSimpson.bind(this)
+
+getSimpson() {
+  axios.get('https://quests.wilders.dev/simpsons-quotes/quotes')
+    .then (response => response.data)
+    .then (data => {
+      this.setState ({
+        simpson: data[0],
+      });
+    });
+}
+
+render (){
+  const {simpson} = this.state
+  return(
+    <div>
+      <QuoteCitation simpson={simpson} />
+      <button type="button" onClick={this.getSimpson}>Get simpson</button>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
